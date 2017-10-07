82a418164a4a8adb8cd5e2b935063015d4e9534d
NinnatCMM <32545477+NinnatCMM@users.noreply.github.com>
2017-10-07 16:14:23 +0700

    Add content to twig

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,5 +1,31 @@
 exports.index = function(req,res) {
-	res.render('home.twig')
+
+	// res.render('home.twig')
+	let content = {
+		highlight: {
+			url:'/assets/images/home/banner/highlight.jpg',
+			title: 'Hello'
+		},
+		thumbnail: [
+			{
+				url:'/assets/images/home/banner/thumbnail-1.jpg',
+				title: 'Vegetable'
+			},
+			{
+				url:'/assets/images/home/banner/thumbnail-2.jpg',
+				title: 'business'
+			},
+			{
+				url:'/assets/images/home/banner/thumbnail-3.jpg',
+				title: 'ingredient'
+			},
+			{
+				url:'/assets/images/home/banner/thumbnail-4.jpg',
+				title: 'playing'
+			}]
+	}
+
+	res.render('home.twig',content)
 }
 // 	let content = {
 // 		banner: [
